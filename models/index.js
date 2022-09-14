9164722812f1b1070a6b2ac1c8980981f37448f2
Shaima Alkhateeb <shaimaalkhateeb535@gmail.com>
2022-09-14 11:42:57 +0300

    fix

--- models/index.js
+++ models/index.js
@@ -6,18 +6,27 @@ const post = require('./post.model');
 // const POSTGRES_URL = process.env.HEROKU_POSTGRESQL_MAUVE_URL || 'postgresql://shaima:0000@localhost:5432/shaima';
 const POSTGRES_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL; // npm i sqlite3
 
-// // ssl
-// const sequelizeOption = {
+// ssl
+const sequelizeOption = {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+};
+
+// let sequelizeOption = process.env.NODE_ENV === 'production' ? {
 //   dialectOptions: {
 //     ssl: {
 //       require: true,
-//       rejectUnauthorized: false
+//       rejectUnauthorized: false,
 //     }
 //   }
-// };
+// } : {};
 
-// let sequelize = new Sequelize(POSTGRES_URL, sequelizeOption);
-let sequelize = new Sequelize(POSTGRES_URL);
+let sequelize = new Sequelize(POSTGRES_URL, sequelizeOption);
+// let sequelize = new Sequelize(POSTGRES_URL);
 // let sequelize = new Sequelize('shaima', 'shaima', '0000', {
 //   host: 'localhost',
 //   dialect: 'postgres'
